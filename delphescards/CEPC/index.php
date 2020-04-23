dd7ffed6dff0dbc7a74ddfa54d03799184890df3
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2020-04-23 23:12:26 +0200

    Initial commit of delphescards and whizard configuration files

---
+++ delphescards/CEPC/index.php
@@ -0,0 +1,17 @@
+<?php
+$thispath = getcwd();
+$thisdir = basename($thispath);
+echo "<p>Delphes Cards for $thisdir :</p>";
+$path = ".";
+$dh = opendir($path);
+$i=1;
+echo "<ul>";
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<li> <a href='$path/$file'>$file</a></li>";
+        $i++;
+    }
+}
+echo "</ul>";
+closedir($dh);
+?> 
